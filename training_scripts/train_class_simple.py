6d96e7f4de12b23ff5c6fa143c28af8023f0ffe3
Isaac <irlyngaas@github.com>
2025-05-07 13:19:18 -0400

    Adding fixed training script. Need to have zero_grad after step

---
+++ training_scripts/train_class_simple.py
@@ -0,0 +1,366 @@
+
+import glob
+import os
+import sys
+import random
+from datetime import timedelta
+import numpy as np
+import torch
+import torch.nn as nn
+from torch.nn.parallel import DistributedDataParallel as DDP
+import torch.distributed as dist
+from torchvision.utils import save_image
+import time
+import yaml
+from einops import rearrange
+
+from UCF_VIT.simple.arch import VIT
+from UCF_VIT.utils.misc import configure_optimizer, configure_scheduler
+from UCF_VIT.dataloaders.datamodule import NativePytorchDataModule
+
+def training_step(data, variables, label, net: VIT):
+
+    output = net.forward(data, variables)
+    criterion = nn.CrossEntropyLoss()
+    loss = criterion(output,label)
+
+    return loss, output
+
+
+def main(device):
+#1. Load arguments from config file and setup parallelization
+##############################################################################################################
+
+    print("in main()","sys.argv[1] ",sys.argv[1],flush=True) 
+    world_size = int(os.environ['SLURM_NTASKS'])
+    world_rank = dist.get_rank()
+
+    config_path = sys.argv[1]
+
+    if world_rank==0:
+        print("config_path ",config_path,flush=True)
+
+    conf = yaml.load(open(config_path,'r'),Loader=yaml.FullLoader)
+
+    if world_rank==0: 
+        print(conf,flush=True)
+
+    max_epochs = conf['trainer']['max_epochs']
+
+    checkpoint_path = conf['trainer']['checkpoint_path']
+  
+    checkpoint_filename = conf['trainer']['checkpoint_filename']
+
+    checkpoint_filename_for_loading = conf['trainer']['checkpoint_filename_for_loading']
+
+    resume_from_checkpoint = conf['trainer']['resume_from_checkpoint']
+ 
+    lr = float(conf['model']['lr'])
+
+    beta_1 = float(conf['model']['beta_1'])
+
+    beta_2 = float(conf['model']['beta_2'])
+
+    weight_decay = float(conf['model']['weight_decay'])
+
+    warmup_steps = conf['model']['warmup_steps']
+
+    max_steps = conf['model']['max_steps']
+
+    warmup_start_lr = float(conf['model']['warmup_start_lr'])
+
+    eta_min = float(conf['model']['eta_min'])
+
+    default_vars =  conf['model']['net']['init_args']['default_vars']
+
+    tile_size = conf['model']['net']['init_args']['tile_size']
+
+    patch_size = conf['model']['net']['init_args']['patch_size']
+ 
+    emb_dim = conf['model']['net']['init_args']['embed_dim']
+
+    depth = conf['model']['net']['init_args']['depth']
+
+    num_heads = conf['model']['net']['init_args']['num_heads']
+    
+    mlp_ratio = conf['model']['net']['init_args']['mlp_ratio']
+
+    drop_path = conf['model']['net']['init_args']['drop_path']
+
+    drop_rate = conf['model']['net']['init_args']['drop_rate']
+
+    twoD = conf['model']['net']['init_args']['twoD']
+
+    use_varemb = conf['model']['net']['init_args']['use_varemb']
+
+    adaptive_patching = conf['model']['net']['init_args']['adaptive_patching']
+
+    if adaptive_patching:
+        fixed_length = conf['model']['net']['init_args']['fixed_length']
+        separate_channels = conf['model']['net']['init_args']['separate_channels']
+        gauss_filter_order = conf['model']['net']['init_args']['gauss_filter_order']
+    else:
+        fixed_length = None
+        separate_channels = None
+        gauss_filter_order = None
+
+    dataset = conf['data']['dataset']
+    assert dataset in ["imagenet"], "This training script only supports imagenet"
+
+
+
+    dict_root_dirs = conf['data']['dict_root_dirs']
+
+    dict_start_idx = conf['data']['dict_start_idx']
+
+    dict_end_idx = conf['data']['dict_end_idx']
+
+    dict_buffer_sizes = conf['data']['dict_buffer_sizes']
+
+    num_channels_available = conf['data']['num_channels_available']
+
+    num_channels_used = conf['data']['num_channels_used']
+
+    dict_in_variables = conf['data']['dict_in_variables']
+
+    batch_size = conf['data']['batch_size']
+
+    num_workers = conf['data']['num_workers']
+
+    pin_memory = conf['data']['pin_memory']
+
+    num_classes = conf['data']['num_classes']
+
+    single_channel = conf['data']['single_channel']
+
+    tile_overlap = conf['data']['tile_overlap']
+
+    use_all_data = conf['data']['use_all_data']
+
+    dataset_group_list = conf['load_balancing']['dataset_group_list']
+
+    batches_per_rank_epoch = conf['load_balancing']['batches_per_rank_epoch']
+
+    tile_size_x = tile_size[0]
+    tile_size_y = tile_size[1]
+
+    if dataset == "imagenet":
+        tile_size_z = None
+    else:
+        tile_size_z = tile_size[2]
+    
+    assert (tile_size_x%patch_size)==0, "tile_size_x % patch_size must be 0"
+    assert (tile_size_y%patch_size)==0, "tile_size_y % patch_size must be 0"
+    if dataset != "imagenet":
+        assert (tile_size_z%patch_size)==0, "tile_size_z % patch_size must be 0"
+
+#2. Initialize model, optimizer, and scheduler
+##############################################################################################################
+    #Find correct in_chans to use
+    if single_channel:
+        max_channels = 1
+    else:
+        max_channels = 1
+        for i,k in enumerate(num_channels_used):
+            if num_channels_used[k] > 1:
+                max_channels = num_channels_used[k]
+
+    model = VIT(
+        img_size=tile_size,
+        patch_size=patch_size,
+        num_classes=num_classes,
+        in_chans=max_channels,
+        embed_dim=emb_dim,
+        depth=depth,
+        num_heads=num_heads,
+        mlp_ratio=mlp_ratio,
+        drop_path_rate=drop_path,
+        drop_rate=drop_rate,
+        twoD=twoD,
+        weight_init='',
+        default_vars=default_vars,
+        single_channel=single_channel,
+        use_varemb=use_varemb,
+        adaptive_patching=adaptive_patching,
+        fixed_length=fixed_length,
+    ).to(device)
+
+    local_rank = int(os.environ['SLURM_LOCALID'])
+    #model = DDP(model,device_ids=[local_rank],output_device=[local_rank])
+    #find_unused_parameters=True is needed under these circumstances
+    model = DDP(model,device_ids=[local_rank],output_device=[local_rank],find_unused_parameters=True)
+ 
+    optimizer = configure_optimizer(model,lr,beta_1,beta_2,weight_decay)
+    scheduler = configure_scheduler(optimizer,warmup_steps,max_steps,warmup_start_lr,eta_min)
+
+    if not resume_from_checkpoint:
+        epoch_start = 0
+        isExist = os.path.exists(checkpoint_path)
+        if not isExist:
+            # Create a new directory because it does not exist
+            os.makedirs(checkpoint_path,exist_ok=True)
+            print("The new checkpoint directory is created!")        
+        loss_list = []
+    else:
+        dist.barrier()
+        #map_location = 'cuda:'+str(device)
+        map_location = 'cpu'
+        checkpoint = torch.load(checkpoint_path+"/"+checkpoint_filename_for_loading+".ckpt",map_location=map_location)
+        model.load_state_dict(checkpoint['model_state_dict'])
+        epoch_start = checkpoint['epoch']
+        epoch_start += 1
+        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+        scheduler.load_state_dict(checkpoint['scheduler_state_dict'])
+        loss_list = checkpoint['loss_list']
+        del checkpoint
+
+
+    dist.barrier()
+
+#3. Initialize Dataloader
+##############################################################################################################
+    data_module = NativePytorchDataModule(dict_root_dirs=dict_root_dirs,
+        dict_start_idx=dict_start_idx,
+        dict_end_idx=dict_end_idx,
+        dict_buffer_sizes=dict_buffer_sizes,
+        dict_in_variables=dict_in_variables,
+        num_channels_available = num_channels_available,
+        num_channels_used = num_channels_used,
+        batch_size=batch_size,
+        num_workers=num_workers,
+        pin_memory=pin_memory,
+        patch_size = patch_size,
+        tile_size_x = tile_size_x,
+        tile_size_y = tile_size_y,
+        tile_size_z = tile_size_z,
+        twoD = twoD,
+        single_channel = single_channel,
+        return_label = True,
+        dataset_group_list = dataset_group_list,
+        batches_per_rank_epoch = batches_per_rank_epoch,
+        tile_overlap = tile_overlap,
+        use_all_data = use_all_data,
+        adaptive_patching = adaptive_patching,
+        fixed_length = fixed_length,
+        separate_channels = separate_channels,
+        gauss_filter_order = gauss_filter_order,
+        data_par_size = dist.get_world_size(),
+        dataset = dataset,
+    ).to(device)
+
+    data_module.setup()
+
+    train_dataloader = data_module.train_dataloader()
+
+#4. Training Loop
+##############################################################################################################
+
+    #Find max batches
+    iterations_per_epoch = 0
+    for i,k in enumerate(batches_per_rank_epoch):
+        if batches_per_rank_epoch[k] > iterations_per_epoch:
+            iterations_per_epoch = batches_per_rank_epoch[k]
+
+    for epoch in range(epoch_start,max_epochs):
+        #tell the model that we are in train mode. Matters because we have the dropout
+        model.train()
+        epoch_loss = torch.tensor(0.0 , dtype=torch.float32, device=device)
+        epoch_accuracy = torch.tensor(0.0 , dtype=torch.float32, device=device)
+        if world_rank==0:
+            print("epoch ",epoch,flush=True)
+
+        counter = 0
+        for batch_idx, batch in enumerate(train_dataloader):
+            counter = counter + 1
+            if counter > iterations_per_epoch:
+                print("A GPU ran out of data, moving to next epoch", flush=True)
+                break
+
+            if adaptive_patching:
+                data, seq, size, pos, label, variables = batch
+                seq = seq.to(device)
+                label = label.to(device)
+                loss, output = training_step(seq, variables, label, model)
+
+            else:
+                data, label, variables = batch
+                data = data.to(device)
+                label = label.to(device)
+                loss, output = training_step(data, variables, label, model)
+
+            acc = (output.argmax(dim=1) == label).float().mean()
+
+            epoch_accuracy += acc.detach()
+            epoch_loss += loss.detach()
+    
+            if world_rank==0:
+                print("epoch: ",epoch, "batch_idx", batch_idx, "it_loss ",loss, "it_acc", acc, flush=True)
+    
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+            scheduler.step()
+        loss_list.append(epoch_loss)
+
+
+        if world_rank==0:
+            print("epoch: ",epoch," epoch_loss ",epoch_loss, "epoch_accuracy ", epoch_accuracy, flush=True)
+
+        model_states = model.state_dict()
+        optimizer_states = optimizer.state_dict()
+        scheduler_states = scheduler.state_dict()
+
+
+        #Alternating saving in to odd and even checkpoint file to avoid losing progress
+        if world_rank == 0 and epoch % 2 == 0:
+     
+            torch.save({
+                'epoch': epoch,
+                'model_state_dict': model_states,
+                'optimizer_state_dict': optimizer_states,
+                'scheduler_state_dict': scheduler_states,
+                'loss_list' : loss_list,
+                }, checkpoint_path+"/"+checkpoint_filename+"_even.ckpt")
+
+        if world_rank == 0 and epoch % 2 == 1:
+
+            torch.save({
+                'epoch': epoch,
+                'model_state_dict': model_states,
+                'optimizer_state_dict': optimizer_states,
+                'scheduler_state_dict': scheduler_states,
+                'loss_list' : loss_list,
+                }, checkpoint_path+"/"+checkpoint_filename+"_odd.ckpt")
+     
+        dist.barrier()
+        del model_states
+        del optimizer_states
+        del scheduler_states
+
+if __name__ == "__main__":
+
+    os.environ['MASTER_ADDR'] = str(os.environ['HOSTNAME'])
+    os.environ['MASTER_PORT'] = "29500"
+    os.environ['WORLD_SIZE'] = os.environ['SLURM_NTASKS']
+    os.environ['RANK'] = os.environ['SLURM_PROCID']
+
+    world_size = int(os.environ['SLURM_NTASKS'])
+    world_rank = int(os.environ['SLURM_PROCID'])
+    local_rank = int(os.environ['SLURM_LOCALID'])
+
+    torch.cuda.set_device(local_rank)
+    device = torch.cuda.current_device()
+
+
+
+    #torch.backends.cudnn.benchmark = True
+
+    dist.init_process_group('nccl', timeout=timedelta(seconds=7200000), rank=world_rank, world_size=world_size)
+
+#    initialize_process()
+
+    print("Using dist.init_process_group. world_size ",world_size,flush=True)
+    
+    main(device)
+
+    dist.destroy_process_group()
