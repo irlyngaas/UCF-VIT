a795dcf1686bab9f5f6d4cbb7f6e5b70acbdfd8a
Isaac <irlyngaas@github.com>
2025-05-22 10:31:08 -0400

    Combining load balancing script into a single file

--- utils/preprocess_load_balancing.py
+++ utils/preprocess_load_balancing.py
@@ -2,11 +2,14 @@ import os
 import sys
 import numpy as np
 import torch
-import torchdata.datapipes as dp
 import yaml
 import nibabel as nib
 from pathlib import Path
 import math
+import glob
+from PIL import Image
+import cv2 as cv
+import torchdata.datapipes as dp
 
 
 def main():
@@ -41,14 +44,47 @@ def main():
     use_all_data = conf['data']['use_all_data']
     patch_size =  conf['model']['net']['init_args']['patch_size']
     max_epochs=conf['trainer']['max_epochs']
+    dataset = conf['data']['dataset']
+
+    if dataset == "imagenet":
+        imagenet_resize = conf['dataset_options']['imagenet_resize']
 
     tile_size_x = int(tile_size[0])
     tile_size_y = int(tile_size[1])
-    tile_size_z = int(tile_size[2])
+    if dataset != "imagenet":
+        tile_size_z = int(tile_size[2])
 
-    dict_lister_trains = {
-        k: list(dp.iter.FileLister(os.path.join(root_dir, "imagesTr"))) for k, root_dir in dict_root_dirs.items()
-    }
+    if dataset == "imagenet":
+        dict_lister_trains = {}
+        for k, root_dir in dict_root_dirs.items():
+            classes = sorted(os.listdir(root_dir))
+            class_to_idx = {cls_name: idx for idx, cls_name in enumerate(classes)}
+            if len(classes) > num_total_ddp_ranks:
+                classes_to_combine = int(len(classes) // num_total_ddp_ranks)
+            img_list = []
+            classes_counter = 0
+            num_data_roots = 0
+            for cls_name in classes: 
+                if classes_counter == classes_to_combine:
+                    classes_counter = 0
+                    img_list = []
+                cls_dir = os.path.join(root_dir, cls_name)
+                for img_path in glob.glob(os.path.join(cls_dir,"*.JPEG")):
+                    img_list.append(img_path)
+                classes_counter += 1
+                if classes_counter == classes_to_combine:
+                    print("LEN_IMG_LIST",len(img_list))
+                    img_dict = {num_data_roots: img_list}
+                    dict_lister_trains.update(img_dict)
+                    num_data_roots +=1
+
+                if num_data_roots > num_total_ddp_ranks-1:
+                    break
+    else:
+        dict_lister_trains = {
+            k: list(dp.iter.FileLister(os.path.join(root_dir, "imagesTr"))) for k, root_dir in dict_root_dirs.items()
+        }
+        
 
     num_total_tiles = []
     num_total_images = []
@@ -56,19 +92,27 @@ def main():
     num_channels_per_dataset = []
     for i, k in enumerate(dict_lister_trains.keys()):
         lister_train = dict_lister_trains[k]
-        start_idx = int(dict_start_idx[k] * len(lister_train))
-        end_idx = int(dict_end_idx[k] * len(lister_train))
+        if dataset == "imagenet":
+            start_idx = int(dict_start_idx["imagenet"] * len(lister_train))
+            end_idx = int(dict_end_idx["imagenet"] * len(lister_train))
+        else:
+            start_idx = int(dict_start_idx[k] * len(lister_train))
+            end_idx = int(dict_end_idx[k] * len(lister_train))
         keys = lister_train[start_idx:end_idx]
         num_total_images.append(len(keys))
 
         #Assume all channels have the same data size
         data_path = keys[0]
-        data = nib.load(data_path)
-        data = np.array(data.dataobj).astype(np.float32)
+        if dataset == "imagenet":
+            data = Image.open(data_path).convert("RGB")
+            data = np.array(data) 
+            data = cv.resize(data, dsize=[imagenet_resize["imagenet"],imagenet_resize["imagenet"]])
+        else:
+            data = nib.load(data_path)
+            data = np.array(data.dataobj).astype(np.float32)
 
         tile_overlap_size_x = int(tile_size_x*tile_overlap)
         tile_overlap_size_y = int(tile_size_y*tile_overlap)
-        tile_overlap_size_z = int(tile_size_z*tile_overlap)
         
         if tile_overlap == 0.0:
             OTP2_x = 1
@@ -82,60 +126,80 @@ def main():
         else:
             OTP2_y = int(tile_size_y/tile_overlap_size_y)
             
-        if tile_overlap == 0.0:
-            OTP2_z = 1
-            tile_overlap_size_z = tile_size_z
-        else:
-            OTP2_z = int(tile_size_z/tile_overlap_size_z)
-
-        #Total Tiles Evenly Spaced
-        TTE_x = data.shape[0]//tile_size_x
-        TTE_y = data.shape[1]//tile_size_y
-        num_blocks_x = (TTE_x-1)*OTP2_x + 1
-        num_blocks_y = (TTE_y-1)*OTP2_y + 1
-        if use_all_data:
-            #Total Tiles
-            TT_x = data.shape[0]/(tile_size_x)
-            TT_y = data.shape[1]/(tile_size_y)
-            # Number of leftover overlap patches for last tile
-            LTOP_x = np.floor((TT_x-TTE_x)*OTP2_x)
-            LTOP_y = np.floor((TT_y-TTE_y)*OTP2_y)
-            if data.shape[0] % tile_overlap_size_x != 0:
-                LTOP_x += 1
-            if data.shape[1] % tile_overlap_size_y != 0:
-                LTOP_y += 1
-            num_blocks_x = int(num_blocks_x + LTOP_x)
-            num_blocks_y = int(num_blocks_y + LTOP_y)
-
-        if twoD:
+        if dataset == "imagenet":
+            #Total Tiles Evenly Spaced
+            TTE_x = data.shape[0]//tile_size_x
+            TTE_y = data.shape[1]//tile_size_y
+            num_blocks_x = (TTE_x-1)*OTP2_x + 1
+            num_blocks_y = (TTE_y-1)*OTP2_y + 1
             if use_all_data:
-                num_blocks_z = data.shape[2]//tile_size_z
-                leftover_z_tiles = data.shape[2] % tile_size_z
-            else:
-                num_blocks_z = data.shape[2]//tile_size_z
-        else:
-            TTE_z = data.shape[2]//tile_size_z
-            num_blocks_z = (TTE_z-1)*OTP2_z + 1
-            if use_all_data:
-                #Total Tiles Rounded Up
-                TT_z = data.shape[2]/(tile_size_z)
+                #Total Tiles
+                TT_x = data.shape[0]/(tile_size_x)
+                TT_y = data.shape[1]/(tile_size_y)
                 # Number of leftover overlap patches for last tile
-                LTOP_z = np.floor((TT_z-TTE_z)*OTP2_z)
-                if data.shape[2] % tile_overlap_size_z != 0:
-                    LTOP_z += 1
-                num_blocks_z = int(num_blocks_z + LTOP_z)
+                LTOP_x = np.floor((TT_x-TTE_x)*OTP2_x)
+                LTOP_y = np.floor((TT_y-TTE_y)*OTP2_y)
+                if data.shape[0] % tile_overlap_size_x != 0:
+                    LTOP_x += 1
+                if data.shape[1] % tile_overlap_size_y != 0:
+                    LTOP_y += 1
+                num_blocks_x = int(num_blocks_x + LTOP_x)
+                num_blocks_y = int(num_blocks_y + LTOP_y)
 
-        print("KEY", k, "DATA_SHAPE", data.shape,"NUM_BLOCKS:", num_blocks_x, num_blocks_y, num_blocks_z, flush=True)
+            print("KEY", k, "DATA_SHAPE", data.shape,"NUM_BLOCKS:", num_blocks_x, num_blocks_y, flush=True)
 
-        if twoD:
-            if use_all_data:
-                tiles_per_image.append(num_blocks_x*num_blocks_y*num_blocks_z*tile_size_z + num_blocks_x*num_blocks_y*leftover_z_tiles)
-            else:
-                tiles_per_image.append(num_blocks_x*num_blocks_y*num_blocks_z*tile_size_z)
+            tiles_per_image.append(num_blocks_x*num_blocks_y)
+            num_channels_per_dataset.append(num_channels_used["imagenet"])
         else:
-            tiles_per_image.append(num_blocks_x*num_blocks_y*num_blocks_z)
-        
-        num_channels_per_dataset.append(num_channels_used[k])
+            #Total Tiles Evenly Spaced
+            TTE_x = data.shape[0]//tile_size_x
+            TTE_y = data.shape[1]//tile_size_y
+            num_blocks_x = (TTE_x-1)*OTP2_x + 1
+            num_blocks_y = (TTE_y-1)*OTP2_y + 1
+            if use_all_data:
+                #Total Tiles
+                TT_x = data.shape[0]/(tile_size_x)
+                TT_y = data.shape[1]/(tile_size_y)
+                # Number of leftover overlap patches for last tile
+                LTOP_x = np.floor((TT_x-TTE_x)*OTP2_x)
+                LTOP_y = np.floor((TT_y-TTE_y)*OTP2_y)
+                if data.shape[0] % tile_overlap_size_x != 0:
+                    LTOP_x += 1
+                if data.shape[1] % tile_overlap_size_y != 0:
+                    LTOP_y += 1
+                num_blocks_x = int(num_blocks_x + LTOP_x)
+                num_blocks_y = int(num_blocks_y + LTOP_y)
+
+            if twoD:
+                if use_all_data:
+                    num_blocks_z = data.shape[2]//tile_size_z
+                    leftover_z_tiles = data.shape[2] % tile_size_z
+                else:
+                    num_blocks_z = data.shape[2]//tile_size_z
+            else:
+                TTE_z = data.shape[2]//tile_size_z
+                num_blocks_z = (TTE_z-1)*OTP2_z + 1
+                if use_all_data:
+                    #Total Tiles Rounded Up
+                    TT_z = data.shape[2]/(tile_size_z)
+                    # Number of leftover overlap patches for last tile
+                    LTOP_z = np.floor((TT_z-TTE_z)*OTP2_z)
+                    if data.shape[2] % tile_overlap_size_z != 0:
+                        LTOP_z += 1
+                    num_blocks_z = int(num_blocks_z + LTOP_z)
+
+            print("KEY", k, "DATA_SHAPE", data.shape,"NUM_BLOCKS:", num_blocks_x, num_blocks_y, num_blocks_z, flush=True)
+
+            if twoD:
+                if use_all_data:
+                    tiles_per_image.append(num_blocks_x*num_blocks_y*num_blocks_z*tile_size_z + num_blocks_x*num_blocks_y*leftover_z_tiles)
+                else:
+                    tiles_per_image.append(num_blocks_x*num_blocks_y*num_blocks_z*tile_size_z)
+            else:
+                tiles_per_image.append(num_blocks_x*num_blocks_y*num_blocks_z)
+            
+            num_channels_per_dataset.append(num_channels_used[k])
+
         if single_channel:
             num_total_tiles.append(tiles_per_image[i] * num_channels_per_dataset[i] * num_total_images[i])
         else:
@@ -164,6 +228,7 @@ def main():
     print("DDP RANK RATIO", ddp_rank_ratio)
 
     rank_sum = sum(ddp_rank_ratio)
+
     #Rebalance till ranks equal actually amount wanted to use
     while rank_sum != num_total_ddp_ranks:
         leftover = []
@@ -226,7 +291,24 @@ def main():
             batches_per_rank.append(np.floor(num_images_per_rank[i])*tiles_per_image[i]/batch_size)
         tiles_per_rank.append(np.floor(num_images_per_rank[i])*tiles_per_image[i])
     print("Tiles Per Rank", tiles_per_rank)
-    print("Batches Per Rank", batches_per_rank)
+    #print("Batches Per Rank", batches_per_rank)
+    #print("Min Batches Per Rank", min(batches_per_rank))
+    print("USE BELOW IN CONFIG FILE")
+    print("batches_per_rank_epoch: {")
+    if dataset == "imagenet":
+        print("'%s': %i," % ("imagenet", int(min(batches_per_rank))))
+    else:
+        for i, k in enumerate(dict_lister_trains.keys()):
+            print("'%s': %i," % (k, int(batches_per_rank[i])))
+    print('}')
+
+    grouplist_str = ''
+    for i in range(len(ddp_rank_ratio)):
+        grouplist_str += str(ddp_rank_ratio[i])+','
+    print("dataset_group_list: '%s'" % (grouplist_str[:-1]))
+
+      
+        
 
 if __name__ == "__main__":
     main()
