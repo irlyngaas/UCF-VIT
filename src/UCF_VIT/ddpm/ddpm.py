dc05dd5db6f86625fa3974c1bba74ecc3b7c6374
NasikNafi <nasik.nafi@gmail.com>
2025-06-06 17:04:36 -0400

    Modification in config, dataloading, inferencing, and model saving

--- src/UCF_VIT/ddpm/ddpm.py
+++ src/UCF_VIT/ddpm/ddpm.py
@@ -28,14 +28,14 @@ def sample_images(model, var, device, res, precision_dt, patch_size, epoch=0, nu
     if not twoD:
         z = torch.randn(num_samples, 1, res[0], res[1], res[2])
     else:
-        z = torch.randn(num_samples, 1, res[0], res[1])
+        z = torch.randn(num_samples, 1, res[1], res[2])
 
     with torch.no_grad():
         for t in reversed(range(1, num_time_steps)):
             if not twoD:
                 e = torch.randn(num_samples, 1, res[0], res[1], res[2])
             else:
-                e = torch.randn(num_samples, 1, res[0], res[1])
+                e = torch.randn(num_samples, 1, res[1], res[2])
 
             t = [t]
             temp = (scheduler.beta[t]/( (torch.sqrt(1-scheduler.alpha[t]))*(torch.sqrt(1-scheduler.beta[t]))))
@@ -59,13 +59,60 @@ def sample_images(model, var, device, res, precision_dt, patch_size, epoch=0, nu
 
     if save_path is not None:
         if not twoD:
-            plot_3D_array_slices(images, filename=os.path.join(save_path, '3D_gen_%s_%i_%i_rank%i.png' %(var, epoch, res[0], dist.get_rank())))
+            plot_3D_array_slices(images, filename=os.path.join(save_path, '3D_gen_%s_%i_%i_%i_%irank%i.png' %(var, epoch, res[0], res[1], res[2], dist.get_rank())))
         else:
             fig,ax = plt.subplots(10,10,figsize=(10,10),facecolor='w')
             for i in range(images[0].shape[0]):
                 for j in range(len(times)):
                     ax[i,j].axis('off')
-                    ax[i,j].imshow(images[j][i,:,:,0], interpolation='none', cmap='gray')
+                    ax[i,j].imshow(images[j][i,:,:,0], interpolation='none')
             plt.subplots_adjust(left=0.01, right=0.99, top=0.99, bottom=0.01, hspace=0.025, wspace=0.025)
-            fig.savefig(os.path.join(save_path,'2D_gen_%s_%i_%i_rank%i.png' %(var, epoch, res[0], dist.get_rank())), format='png', dpi=300)
+            fig.savefig(os.path.join(save_path,'2D_gen_%s_%i_%i_%i_rank%i.png' %(var, epoch, res[1], res[2], dist.get_rank())), format='png', dpi=300)
             plt.close(fig)
+
+
+def save_intermediate_data(model, var, device, res, precision_dt, patch_size, epoch=0, num_samples=10, twoD=False, save_path='figures', num_time_steps=1000):
+
+    scheduler = DDPM_Scheduler(num_time_steps=num_time_steps)
+    times = [0, 15, 50, 100, 200, 300, 400, 550, 700, 999]
+
+    images = []
+    if not twoD:
+        z = torch.randn(num_samples, 1, res[0], res[1], res[2])
+    else:
+        z = torch.randn(num_samples, 1, res[1], res[2])
+
+    with torch.no_grad():
+        for t in reversed(range(1, num_time_steps)):
+            if not twoD:
+                e = torch.randn(num_samples, 1, res[0], res[1], res[2])
+            else:
+                e = torch.randn(num_samples, 1, res[1], res[2])
+
+            t = [t]
+            temp = (scheduler.beta[t]/( (torch.sqrt(1-scheduler.alpha[t]))*(torch.sqrt(1-scheduler.beta[t]))))
+            predicted_noise = model(z.to(precision_dt).to(device), torch.tensor(t).to(device), [var]) 
+            predicted_noise = unpatchify(predicted_noise, z.to(precision_dt).to(device), patch_size, twoD)
+            z = (1/(torch.sqrt(1-scheduler.beta[t])))*z - (temp*predicted_noise.cpu())
+            if t[0] in times:
+                images.append(z)
+            z = z + (e*torch.sqrt(scheduler.beta[t]))
+
+        temp = scheduler.beta[0]/( (torch.sqrt(1-scheduler.alpha[0]))*(torch.sqrt(1-scheduler.beta[0])))
+        predicted_noise = model(z.to(precision_dt).to(device), torch.tensor([0]).to(device), [var])
+        predicted_noise = unpatchify(predicted_noise, z.to(precision_dt).to(device), patch_size, twoD)
+        x = (1/(torch.sqrt(1-scheduler.beta[0])))*z - (temp*predicted_noise.cpu())
+        images.append(x)
+
+    if not twoD:
+        images = [rearrange(img.squeeze(0), 'b c h w d -> b h w d c').detach().numpy() for img in images]
+    else:
+        images = [rearrange(img.squeeze(0), 'b c h w -> b h w c').detach().numpy() for img in images]
+    
+    images = np.array(images)
+    images = images.astype('float32')
+
+    if not twoD:
+        np.savez(os.path.join(save_path,'Output_gen_%s_%i_%i_%i_rank%i.npz' %(var, epoch, res[1], res[2], dist.get_rank())),images)
+    else:
+        np.savez(os.path.join(save_path,'Output_gen_%s_%i_%i_%i_%i_rank%i.npz' %(var, epoch, res[0], res[1], res[2], dist.get_rank())),images)
