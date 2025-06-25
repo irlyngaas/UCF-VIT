5023becb35f92e54828a51879a190209166744cb
lyngaasir <lyngaasir@slogin002.cm.cluster>
2025-06-25 10:54:44 -0700

    Adding launch and config for running on dgx

---
+++ launch/xct/diffusion_fsdp_dgx.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --partition defq
+#SBATCH --nodes 2
+#SBATCH --exclusive
+#SBATCH --job-name=diffusion_fsdp
+#SBATCH --gpus-per-node=8
+#SBATCH --ntasks-per-node=8
+
+#[ -z $JOBID ] && JOBID=$SLURM_JOB_ID
+#[ -z $JOBSIZE ] && JOBSIZE=$SLURM_JOB_NUM_NODES
+
+#ulimit -n 65536
+
+srun --mpi=pmix --container-mounts /lustre/fs0 --container-mount-home --container-image /lustre/fs0/scratch/lyngaasir/sqsh-files/0698614322576143+ucf-vit+25.05.sqsh python $HOME/UCF-VIT/dev_scripts/train_diffusion_fsdp.py $HOME/UCF-VIT/configs/xct/diffusion/base_config_dgx.yaml
+
