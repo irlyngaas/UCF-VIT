7136588922fff8c37d048805c581d58f8f410335
Isaac <irlyngaas@github.com>
2025-06-30 13:39:39 -0400

    Adding a slurm script for running on dgx

---
+++ launch/imagenet/classification_simple_dgx.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH --partition defq
+#SBATCH --nodes 1
+#SBATCH --exclusive
+#SBATCH --job-name=diffusion_fsdp
+#SBATCH --gpus-per-node=8
+#SBATCH --ntasks-per-node=8
+
+srun --mpi=pmix --container-mounts /lustre/fs0 --container-mount-home --container-image irlyngaas/irlyng/ucf-vit:25.05 python $HOME/UCF-VIT/training_scripts/train_diffusion_fsdp.py $HOME/UCF-VIT/configs/xct/diffusion/base_config.yaml
