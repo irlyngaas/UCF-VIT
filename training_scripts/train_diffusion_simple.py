385181a59808e14c1d1484b5efcfb9a2184a041a
Isaac <irlyngaas@github.com>
2025-07-08 16:32:03 -0400

    Adding MPI launch capability to training scripts

--- training_scripts/train_diffusion_simple.py
+++ training_scripts/train_diffusion_simple.py
@@ -37,7 +37,7 @@ def main(device):
 ##############################################################################################################
 
     print("in main()","sys.argv[1] ",sys.argv[1],flush=True) 
-    world_size = int(os.environ['SLURM_NTASKS'])
+    world_size = dist.get_world_size()
     world_rank = dist.get_rank()
 
     config_path = sys.argv[1]
@@ -382,25 +382,50 @@ def main(device):
 
 if __name__ == "__main__":
 
-    os.environ['MASTER_ADDR'] = str(os.environ['HOSTNAME'])
+    if len(sys.argv) > 2:
+        LAUNCHER = sys.argv[2]
+    else:
+        LAUNCHER = None
+
+    if LAUNCHER == "MPI":
+        from mpi4py import MPI
+        import socket 
+
+        num_gpus_per_node = torch.cuda.device_count()
+        comm = MPI.COMM_WORLD
+        world_size = comm.Get_size()
+        world_rank = rank = comm.Get_rank()
+        local_rank = int(rank) % int(num_gpus_per_node) if num_gpus_per_node>0 else 0 # local_rank and device are 0 when using 1 GPU per task
+        os.environ['WORLD_SIZE'] = str(world_size)
+        os.environ['RANK'] = str(world_rank)
+        os.environ['LOCAL_RANK'] = str(local_rank)
+
+        master_addr = None
+        if rank == 0:
+            hostname = socket.gethostname()
+            ip_address = socket.gethostbyname(hostname)
+            master_addr = ip_address
+        master_addr = comm.bcast(master_addr, root=0)
+        os.environ['MASTER_ADDR'] = master_addr
+
+        torch.cuda.set_device(local_rank)
+        device = torch.device(local_rank) if torch.cuda.is_available() else torch.device("cpu")
+
+    else:#elif LAUNCHER == "SLURM":
+
+        os.environ['MASTER_ADDR'] = str(os.environ['HOSTNAME'])
+        os.environ['WORLD_SIZE'] = os.environ['SLURM_NTASKS']
+        os.environ['RANK'] = os.environ['SLURM_PROCID']
+
+        world_size = int(os.environ['SLURM_NTASKS'])
+        world_rank = int(os.environ['SLURM_PROCID'])
+        local_rank = int(os.environ['SLURM_LOCALID'])
+
+        torch.cuda.set_device(local_rank)
+        device = torch.cuda.current_device()
+
     os.environ['MASTER_PORT'] = "29500"
-    os.environ['WORLD_SIZE'] = os.environ['SLURM_NTASKS']
-    os.environ['RANK'] = os.environ['SLURM_PROCID']
-
-    world_size = int(os.environ['SLURM_NTASKS'])
-    world_rank = int(os.environ['SLURM_PROCID'])
-    local_rank = int(os.environ['SLURM_LOCALID'])
-
-    torch.cuda.set_device(local_rank)
-    device = torch.cuda.current_device()
-
-
-
-    #torch.backends.cudnn.benchmark = True
-
     dist.init_process_group('nccl', timeout=timedelta(seconds=7200000), rank=world_rank, world_size=world_size)
-
-#    initialize_process()
 
     print("Using dist.init_process_group. world_size ",world_size,flush=True)
     
