503347569b21529a3ade448d92767b13a37f99d2
Isaac <irlyngaas@github.com>
2025-07-17 14:04:40 -0400

    Adding Variable Mapping Attention. Using proper XFormers fused_attention now

--- src/UCF_VIT/fsdp/arch.py
+++ src/UCF_VIT/fsdp/arch.py
@@ -14,7 +14,8 @@ import torch.nn as nn
 from .building_blocks import Block, PatchEmbed, Mlp, DropPath, AttentionPoolLatent, PatchDropout, \
     trunc_normal_, resample_patch_embed, resample_abs_pos_embed, \
     get_act_layer, get_norm_layer, LayerType, \
-    MyUnetBlock, EmbeddingDenseLayer
+    MyUnetBlock, EmbeddingDenseLayer, \
+    VariableMapping_Attention
 
 from timm.models._manipulate import named_apply, checkpoint_seq
 
@@ -305,7 +306,8 @@ class VIT(nn.Module):
             else:
                 self.var_query = nn.Parameter(torch.zeros(1, self.aggregated_variables, self.embed_dim), requires_grad=True)
                 #TODO: Different parameter for specifying num_heads in var_agg rather than encoder num_heads
-                self.var_agg = nn.MultiheadAttention(self.embed_dim, self.num_heads, batch_first=True)
+                #self.var_agg = nn.MultiheadAttention(self.embed_dim, self.num_heads, batch_first=True)
+                self.var_agg = VariableMapping_Attention(self.embed_dim, fused_attn=self.FusedAttn_option, num_heads=self.num_heads, qkv_bias=False, tensor_par_size = self.tensor_par_size, tensor_par_group = self.tensor_par_group)
 
         if weight_init != 'skip':
             self.init_weights('')
@@ -395,8 +397,10 @@ class VIT(nn.Module):
         x = torch.einsum("bvld->blvd", x)
         x = x.flatten(0, 1)  # BxL, V, D
 
-        var_query = self.var_query.repeat_interleave(x.shape[0], dim=0)
-        x , _ = self.var_agg(var_query, x, x)  # BxL, V~ , D, where V~ is the aggregated variables
+        #var_query = self.var_query.repeat_interleave(x.shape[0], dim=0)
+        #x , _ = self.var_agg(var_query, x, x)  # BxL, V~ , D, where V~ is the aggregated variables
+        var_query = self.var_query.expand(x.shape[0], -1, -1).contiguous()
+        x = self.var_agg(var_query, x)  # BxL, V~ , D, where V~ is the aggregated variables
         x = x.squeeze()
 
         if self.tensor_par_size > 1:
--- src/UCF_VIT/simple/building_blocks.py
+++ src/UCF_VIT/simple/building_blocks.py
@@ -294,3 +294,77 @@ class EmbeddingDenseLayer(nn.Module):
     #input here is gonna be just [B,C]
     def forward(self, x):
         return(self.linear2(self.dropout(self.relu(self.linear1(x)))))
+
+class VariableMapping_Attention(nn.Module):
+    def __init__(
+            self,
+            dim: int,
+            fused_attn: FusedAttn = FusedAttn.NONE,
+            num_heads: int = 8,
+            qkv_bias: bool = False,
+            qk_norm: bool = False,
+            proj_bias: bool = True,
+            attn_drop: float = 0.,
+            proj_drop: float = 0.,
+            norm_layer: nn.Module = nn.LayerNorm,
+    ) -> None:
+        super().__init__()
+        assert dim % num_heads == 0, 'dim should be divisible by num_heads'
+        self.num_heads = num_heads
+        self.head_dim = dim // num_heads
+        self.scale = self.head_dim ** -0.5
+        self.fused_attn = fused_attn
+
+        self.q = nn.Linear(dim, dim, bias=qkv_bias)
+
+        self.kv = nn.Linear(dim, dim * 2, bias=qkv_bias)
+
+        self.q_norm = norm_layer(self.head_dim) if qk_norm else nn.Identity()
+        self.k_norm = norm_layer(self.head_dim) if qk_norm else nn.Identity()
+        self.attn_drop = nn.Dropout(attn_drop)
+        self.proj = nn.Linear(dim, dim, bias=proj_bias)
+        self.proj_drop = nn.Dropout(proj_drop)
+
+    def forward(self, var_query: torch.Tensor, x: torch.Tensor) -> torch.Tensor:
+
+        N_a = var_query.size(dim=1) #number of aggregated variables
+        B, N_i, C = x.shape #B batch times sequence length, #N_i number of input variables, C embedding size
+
+        q = self.q(var_query).reshape(B, N_a, self.num_heads, self.head_dim ).permute(0, 2, 1, 3)
+
+        kv = self.kv(x).reshape(B, N_i, 2, self.num_heads, self.head_dim).permute(2, 0, 3, 1, 4)
+
+        k, v = kv.unbind(0)
+        q, k = self.q_norm(q), self.k_norm(k)
+
+        if self.fused_attn == FusedAttn.FLASH:
+            x = xformers.ops.memory_efficient_attention(
+                q.transpose(1, 2), k.transpose(1, 2), v.transpose(1, 2), 
+                p=self.attn_drop.p,
+                op=xformers.ops.MemoryEfficientAttentionFlashAttentionOp
+            )
+        elif self.fused_attn == FusedAttn.CK:
+            x = xformers.ops.memory_efficient_attention(
+                q.transpose(1, 2), k.transpose(1, 2), v.transpose(1, 2),
+                p=self.attn_drop.p,
+                op=xformers.ops.MemoryEfficientAttentionCkOp
+            )
+        elif self.fused_attn == FusedAttn.DEFAULT:
+            x = F.scaled_dot_product_attention(
+                q, k, v,
+                dropout_p=self.attn_drop.p if self.training else 0.,
+            )
+            x = x.transpose(1, 2)
+        else: # FusedAttn.NONE
+            q = q * self.scale
+            attn = q @ k.transpose(-2, -1)
+            attn = attn.softmax(dim=-1)
+            attn = self.attn_drop(attn)
+            x = attn @ v
+            x = x.transpose(1, 2)
+
+        x = x.reshape(B, N_a, C)
+        x = self.proj(x)
+        x = self.proj_drop(x)
+
+        return  x
